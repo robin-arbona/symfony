5cfde2fe50d975e892ea7767871fb7d83bb33911
Arbona Robin <71873082+robin-arbona@users.noreply.github.com>
2021-09-22 11:22:27 +0200

    Relativ path to blog article are working

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Post;
 
 class HomeController extends AbstractController
 {
@@ -18,6 +19,16 @@ class HomeController extends AbstractController
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'posts' => $posts,
+        ]);
+    }
+
+    /**
+     * @Route("/post/{id}", name="post")
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('home/post.html.twig', [
+            'post' => $post,
         ]);
     }
 }
